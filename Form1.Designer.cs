a0f3248e9dfdc1b926917debc87676eafebd94bc
Sergio Marotco <sergiomarotco@gmail.com>
2021-03-26 00:47:55 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace Marotco_generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_container = new System.Windows.Forms.TextBox();
+            this.textBox_ZIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_pas_maxL = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Пароль на контейнер с ключом:";
+            // 
+            // textBox_container
+            // 
+            this.textBox_container.Location = new System.Drawing.Point(15, 29);
+            this.textBox_container.Name = "textBox_container";
+            this.textBox_container.Size = new System.Drawing.Size(266, 22);
+            this.textBox_container.TabIndex = 2;
+            // 
+            // textBox_ZIP
+            // 
+            this.textBox_ZIP.Location = new System.Drawing.Point(15, 87);
+            this.textBox_ZIP.Name = "textBox_ZIP";
+            this.textBox_ZIP.Size = new System.Drawing.Size(266, 22);
+            this.textBox_ZIP.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(269, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль на архив с контейнером ключа:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "Подсказка";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Длина пароля:";
+            // 
+            // textBox_pas_maxL
+            // 
+            this.textBox_pas_maxL.Location = new System.Drawing.Point(125, 133);
+            this.textBox_pas_maxL.Name = "textBox_pas_maxL";
+            this.textBox_pas_maxL.Size = new System.Drawing.Size(32, 22);
+            this.textBox_pas_maxL.TabIndex = 9;
+            this.textBox_pas_maxL.Text = "16";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(163, 124);
+            this.trackBar1.Maximum = 18;
+            this.trackBar1.Minimum = 12;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 56);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.Value = 14;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(345, 136);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(40, 17);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Сайт";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::Marotco_generator.Properties.Resources.file_find__high_;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(348, 79);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(38, 38);
+            this.button4.TabIndex = 12;
+            this.toolTip1.SetToolTip(this.button4, "Сгенерировать файл");
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(295, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 38);
+            this.button2.TabIndex = 7;
+            this.toolTip1.SetToolTip(this.button2, "Сгенерировать пароль на архив");
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(295, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(38, 38);
+            this.button3.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.button3, "Сгенерировать пароль на контейнер с ключом");
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Marotco_generator.Properties.Resources.file_find__high_;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(348, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 38);
+            this.button1.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.button1, "Сгенерировать файл");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 175);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox_pas_maxL);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox_ZIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_container);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Генератор криптоконверта";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_container;
+        private System.Windows.Forms.TextBox textBox_ZIP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_pas_maxL;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,267 @@
+﻿using PasswordGenerator;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using System.IO;
+
+namespace Marotco_generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        public Microsoft.Office.Interop.Word.Application app;
+        Document doc;
+        public static Object missing = Type.Missing;
+        /// <summary>
+        /// Функция замены строк в word файле 
+        /// </summary>
+        /// <param name="str_old">Заменяемая страка</param>
+        /// <param name="str_new">Строка которой заменяют</param>
+        public void FindReplace(string str_old, string str_new)
+        {
+            object missingObject = null;
+            object item = WdGoToItem.wdGoToPage;
+            object whichItem = WdGoToDirection.wdGoToFirst;
+            object replaceAll = WdReplace.wdReplaceAll;
+            object forward = true;
+            object matchAllWord = true;
+            object matchCase = false;
+            object originalText = str_old;
+            object replaceText = str_new;
+
+            doc.GoTo(ref item, ref whichItem, ref missingObject, ref missingObject);
+            foreach (Range rng in doc.StoryRanges)
+            {
+                rng.Find.Execute(ref originalText, ref matchCase,
+                ref matchAllWord, ref missingObject, ref missingObject, ref missingObject, ref forward,
+                ref missingObject, ref missingObject, ref replaceText, ref replaceAll, ref missingObject,
+                ref missingObject, ref missingObject, ref missingObject);
+            }
+            /* Код работающий быстрее но не работающий с колонтитулами:
+            Find find = app.Selection.Find;
+
+            find.Text = str_old; // текст поиска
+            find.Replacement.Text = str_new; // текст замены
+            find.Execute(FindText: str_old, MatchCase: false, MatchWholeWord: true, MatchWildcards: false,
+                        MatchSoundsLike: missing, MatchAllWordForms: false, Forward: true, Wrap: WdFindWrap.wdFindContinue,
+                        Format: false, ReplaceWith: str_new, Replace: WdReplace.wdReplaceAll);
+            object matchCase = true; object matchWholeWord = true;
+            object matchWildCards = false; object matchSoundLike = false;
+            object nmatchAllForms = false; object forward = true;
+            object format = false; object matchKashida = false;
+            object matchDiactitics = false; object matchAlefHamza = false;
+            object matchControl = false; object read_only = false;
+            object visible = true; object replace = 2;
+            object wrap = 1;
+            foreach (Range range in doc.StoryRanges)
+            {
+                if (range.StoryType == WdStoryType.wdEvenPagesHeaderStory)
+                {
+                    find.Execute(FindText: str_new, MatchCase: false, MatchWholeWord: false, MatchWildcards: false,
+                    MatchSoundsLike: missing, MatchAllWordForms: false, Forward: true, Wrap: WdFindWrap.wdFindContinue,
+                    Format: false, ReplaceWith: str_new, Replace: WdReplace.wdReplaceAll);
+                }
+            }*/
+        }
+        /// <summary>
+        /// Сгенериратьлист с паролем
+        /// </summary>
+        /// <param name="type">true - пароль на контейнер, false - на zip архив</param>
+        public void FileGenerate(bool type)
+        {
+            switch (type)
+            {
+                case true:
+                    if (textBox_container.Text.Length == 0)
+                    {
+                        Gen_container_pass(true);
+                    }
+                    break;
+                case false:
+                    if (textBox_ZIP.Text.Length == 0)
+                    {
+                        Gen_container_pass(false);
+                    }
+                    break;
+                default:
+                    break;
+            }            
+            MessageBox.Show("!После закрытия окна, все процессы winword.exe будут убиты без сохранения!");
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            foreach (var proc in Process.GetProcessesByName("winWord"))
+            {
+                proc.Kill();
+            }
+            //открываем в word документ
+            app = new Microsoft.Office.Interop.Word.Application();
+            File.WriteAllBytes("Template.docx", Properties.Resources.Template.ToArray());
+            doc = app.Documents.Open(Directory.GetCurrentDirectory().ToString() + @"\\Template.docx");
+            int p = 0;
+            char[] password=new char[0];
+            switch (type)
+            {
+                case true:
+                    password = textBox_container.Text.ToCharArray();
+                    break;
+                case false:
+                    password = textBox_ZIP.Text.ToCharArray();
+                    break;
+                default:
+                    break;
+            }
+
+            for (int i = 0; i < 18; i++)
+            {
+                if (i < trackBar1.Value)
+                {
+                    FindReplace("[" + i + "]", password[p].ToString());//выполняем в тексте документа замену текста
+                    p++;
+                }
+                else
+                {
+                    FindReplace("[" + i + "]", "");//выполняем в тексте документа замену текста
+                }
+            }
+            switch (type)
+            {
+                case true:
+                    FindReplace("[от]", "КОНТЕЙНЕРА");//выполняем в тексте документа замену текста   
+                    Clipboard.SetData(DataFormats.Text, (Object)textBox_container.Text);    //копируем в буфер
+                    break;
+                case false:
+                    FindReplace("[от]", "ZIP архива");//выполняем в тексте документа замену текста
+                    Clipboard.SetData(DataFormats.Text, (Object)textBox_ZIP.Text);    //копируем в буфер
+                    break;
+                default:
+                    break;
+            }
+            SaveCloseFile(true);//закрываем открытый в word документ
+            File.Delete(Directory.GetCurrentDirectory().ToString() + @"\\Template.docx");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileGenerate(true);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FileGenerate(false);
+        }
+        /// <summary>
+        /// Путь к файлу с паролем от контейнера
+        /// </summary>
+        string file_container = "";
+        /// <summary>
+        /// Путь к файлу с паролем от ZIP архива
+        /// </summary>
+        string file_container_ZIP = "";
+        /// <summary>
+        /// Закрытие Word и открытие файла
+        /// </summary>
+        /// <param name="type">true - пароль на контейнер, false - на zip архив</param>
+        public void SaveCloseFile(bool type)
+        {
+            DateTime d = DateTime.Now;
+            string CD = Directory.GetCurrentDirectory().ToString();
+            string time2 = d.Year.ToString() + "." + d.Month + "." + d.Day + " " + d.Hour + "-" + d.Minute;
+            switch (type)
+            {
+                case true:
+                    file_container = CD + @"\\" + time2 + " Пароль_на_контейнер.docx"; // новый файл на основе файла-шаблона
+                    app.ActiveDocument.SaveAs(file_container);
+                    app.ActiveDocument.Close();
+                    app.Quit();
+                    app = null;
+                    Process.Start(file_container);
+                    break;
+                case false:
+                    file_container_ZIP = CD + @"\\" + time2 + " Пароль_на_архив.docx"; // новый файл на основе файла-шаблона;
+                    app.ActiveDocument.SaveAs(file_container_ZIP);
+                    app.ActiveDocument.Close();
+                    app.Quit();
+                    app = null;
+                    Process.Start(file_container_ZIP);
+                    break;
+                default:
+                    break;
+            }
+        }
+        /// <summary>
+        /// Сгенерировать пароль и записать в textbox
+        /// </summary>
+        /// <param name="type">true - пароль на контейнер, false - на zip архив</param>
+        private void Gen_container_pass(bool type)
+        {
+            var pwd = new Password(Convert.ToInt32(textBox_pas_maxL.Text)); //настраиваем генератор пароля
+            string result = pwd.Next();                             //генерируем пароль
+            result = result.Replace('i','*');//замена плохочитаемых символов
+            result = result.Replace('I', '?');
+            result = result.Replace('L', '#');
+            result = result.Replace('|', '(');
+            result = result.Replace('o', '@');
+            result = result.Replace('O', '!');
+            char[] resault_char = result.ToArray();
+            for (int i = 4; i < resault_char.Length; i+=5)
+            {
+                resault_char[i] = '-';
+            }
+            result = new string(resault_char);
+            switch (type)//записываем пароль
+            {
+                case true:
+                    textBox_container.Text = result;
+                    break;
+                case false:
+                    textBox_ZIP.Text = result;
+                    break;
+                default:
+                    break;
+            }
+            Clipboard.SetData(DataFormats.Text, (Object)result);    //копируем в буфер
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Gen_container_pass(true);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Gen_container_pass(false);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Icon = Properties.Resources.logo;
+            textBox_pas_maxL.Text = trackBar1.Value.ToString();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            textBox_pas_maxL.Text = trackBar1.Value.ToString();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/sergiomarotco/");
+        }
+
+
+    }
+}
